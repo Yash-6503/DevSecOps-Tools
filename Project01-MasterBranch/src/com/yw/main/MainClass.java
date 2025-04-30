6b7c52e5a433e241a27e3cba9d9fc993d037db99
Yash-6503 <yashwalke6503@gmail.com>
2025-05-01 00:56:39 +0530

    TL made changes to Project

--- Project01-MasterBranch/src/com/yw/main/MainClass.java
+++ Project01-MasterBranch/src/com/yw/main/MainClass.java
@@ -8,9 +8,7 @@ public class MainClass
 	public static void main(String[] args) 
 	{
 		UpiPayment upi = new UpiPayment();
-		
-		BalanceEnquiry balance = new BalanceEnquiry(60000.488);
-		
+				
 		System.out.println(upi.upiPayment(987654321, 911111111, 5000.55));
 		
 		System.out.println(upi.netBanking(44553322, 88557766, 80000.35));
