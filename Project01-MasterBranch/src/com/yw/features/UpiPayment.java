ebe0880e9e3dcb705462cbbc81e40b928221a30b
Yash-6503 <yashwalke6503@gmail.com>
2025-05-01 00:15:29 +0530

    TL Releases Version 1.0 of Upi Payment

--- Project01-MasterBranch/src/com/yw/features/UpiPayment.java
+++ Project01-MasterBranch/src/com/yw/features/UpiPayment.java
@@ -2,7 +2,7 @@ package com.yw.features;
 
 public class UpiPayment 
 {
-	//upi payment feature
+	//upi payment feature version 1.0
 	public String upiPayment(long fromPhone, long toPhone, double amount) {
 		return amount+" transferred from "+fromPhone+" to phone "+toPhone;
 	}
