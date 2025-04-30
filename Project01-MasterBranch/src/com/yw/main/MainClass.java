ad0329900ccc23244b04fd230b3d946e6b85a38f
Yash-6503 <yashwalke6503@gmail.com>
2025-05-01 00:51:00 +0530

    TL Releases Version 2.0 of NetBanking Payment

--- Project01-MasterBranch/src/com/yw/main/MainClass.java
+++ Project01-MasterBranch/src/com/yw/main/MainClass.java
@@ -10,7 +10,7 @@ public class MainClass
 		
 		System.out.println(upi.upiPayment(987654321, 911111111, 5000.55));
 		
-		System.out.println(upi.upiPayment(44553322, 88557766, 80000.35));
+		System.out.println(upi.netBanking(44553322, 88557766, 80000.35));
 	}
 
 }
