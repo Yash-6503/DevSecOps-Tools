c0d950a358a9e34e60295abf813730aea60016cf
Yash-6503 <yashwalke6503@gmail.com>
2025-05-01 01:05:02 +0530

    Developer added new check balance feature

--- Project01-MasterBranch/src/com/yw/Balance/BalanceEnquiry.java
+++ Project01-MasterBranch/src/com/yw/Balance/BalanceEnquiry.java
@@ -3,5 +3,15 @@ package com.yw.Balance;
 public class BalanceEnquiry 
 {
 	//Task1 implement new check balance feature
+	private double balance;
 	
+	public BalanceEnquiry(double balance) {
+		this.balance = balance;
+	}
+	
+	//check balance feature
+	public double checkBalance() {
+		return this.balance;
+	}
+
 }
--- Project01-MasterBranch/src/com/yw/main/MainClass.java
+++ Project01-MasterBranch/src/com/yw/main/MainClass.java
@@ -1,5 +1,6 @@
 package com.yw.main;
 
+import com.yw.Balance.BalanceEnquiry;
 import com.yw.features.UpiPayment;
 
 public class MainClass 
@@ -7,11 +8,14 @@ public class MainClass
 	public static void main(String[] args) 
 	{
 		UpiPayment upi = new UpiPayment();
+		
+		BalanceEnquiry balance = new BalanceEnquiry(60000.789);
 				
 		System.out.println(upi.upiPayment(987654321, 911111111, 5000.55));
 		
 		System.out.println(upi.netBanking(44553322, 88557766, 80000.35));
 		
+		System.out.println("Your Account Balance is : "+balance.checkBalance());
 	}
 
 }
