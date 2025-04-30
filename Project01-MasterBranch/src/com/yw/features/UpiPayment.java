a9979aa1dfb73e77c44b7cd4bf890f6128397acc
Yash-6503 <yashwalke6503@gmail.com>
2025-04-30 23:56:53 +0530

    TL Reviewed Feature

--- Project01-MasterBranch/src/com/yw/features/UpiPayment.java
+++ Project01-MasterBranch/src/com/yw/features/UpiPayment.java
@@ -2,6 +2,7 @@ package com.yw.features;
 
 public class UpiPayment 
 {
+	//upi payment feature
 	public String upiPayment(long phone, double amount) {
 		return amount+" transferred to phone "+phone;
 	}
