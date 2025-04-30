8fa8b2729e2c80c6ff5532cbfa9a1e9e34b55065
Yash-6503 <yashwalke6503@gmail.com>
2025-05-01 01:39:33 +0530

    Project02 - Spring Boot Application

---
+++ Project02-SpringBootApp/src/main/java/com/yw/Project02SpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.yw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project02SpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project02SpringBootAppApplication.class, args);
+	}
+
+}
